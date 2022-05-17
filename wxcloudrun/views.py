123d3ae4111c9492099452104dbc580478223f3d
KongKing <konglinghui0318@aliyun.com>
2022-05-17 11:42:09 +0800

    添加蛋糕相关接口

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -74,15 +74,15 @@ def get_count():
 
 @app.route('/api/cake/get_by_botid_and_name', methods=['POST'])
 def cake_get_by_botid_and_name():
-    bot_id = request.values.get("bot_id")
-    name = request.values.get("name")
-    logger.info("cake_get_by_botid_and_name bot_id= {} ".format(bot_id))
-    logger.info("cake_get_by_botid_and_name name= {} ".format(name))
+
+    params = request.get_json()
+    bot_id = params['bot_id']
+    name = params['name']
 
     cake = query_cake_by_botid_and_name(bot_id, name)
 
     if cake is not None:
-        data = [{'cake_price': 100}]
+        data = [{'cake_price': cake.price}]
         return make_succ_response(data)
     else:
         return make_err_response("不存在此数据")
